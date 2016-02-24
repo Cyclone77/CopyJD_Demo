f05cacf0be1916d0488c5de38a00717566713039
Cyclone77 <fillchar@live.com>
2016-02-24 10:59:59 +0800

    临摹京东首页
    临摹京东首页

---
+++ View/Js/index.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+    //城市列表
+    $("#divTopMenuCon .dorpdown").mouseover(function () {
+        $(this).addClass('hover');
+        $("#divTopMenuCon .icon").removeClass("fa-angle-down").addClass("fa-angle-up");
+    }).mouseout(function () {
+        $(this).removeClass('hover');
+        $("#divTopMenuCon .icon").removeClass("fa-angle-up").addClass("fa-angle-down");
+    });
+    $("#divTopMenuCon .dorpdown-layer li a").click(function () {
+        $("#divTopMenuCon .dorpdown-layer li a").removeClass("selected");
+        $(this).addClass("selected");
+        $("#myCity").text($(this).text());
+    })
+})
